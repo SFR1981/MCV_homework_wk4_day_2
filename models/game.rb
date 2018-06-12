03ad71b1945521856480439e97ce3d966f75393e
Stephen Rooney <s.f.rooney@gmail.com>
2018-06-12 18:40:18 +0100

    full on regex solution

--- models/game.rb
+++ models/game.rb
@@ -6,11 +6,18 @@ class Game
   end
 
   def rps
-    string = "#{@player1[0]}#{@player2[0]}"
 
-  return "It is a good draw!" if @player1 == @player2
-  return "player 1 wins with #{@player1}" if string.match?(/rs||sp||pr/)
-  return "player 2 wins with #{@player2}" if string.match?(/rp||sr||ps/)
+
+  string = "#{@player1}#{@player2}"
+
+  return "It is a draw!" if @player1 == @player2
+  return "player 1 wins with #{@player1}" if string.match?(/rockscissors|scissorspaper|paperrock/)
+  return "player 2 wins with #{@player2}" if string.match?(/rockpaper|scissorsrock|papersscissors/)
+  return "invalid entry"
+
+
+
+
 
 
 
